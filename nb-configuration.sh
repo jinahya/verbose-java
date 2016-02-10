007c52b0c746e7c2144cbfe0fb924cb962e6086a
Jin Kwon <onacit@gmail.com>
2016-02-10 19:43:40 +0900

    updated

--- nb-configuration.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find . -mindepth 2 -name pom.xml -execdir pwd \; -execdir cp -v ../nb-configuration.xml . \;
--- verbose-java-book/pdf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mvn asciidoctor:process-asciidoc@pdf-cover asciidoctor:process-asciidoc@pdf-book asciidoctor:process-asciidoc@pdf-chapter01
