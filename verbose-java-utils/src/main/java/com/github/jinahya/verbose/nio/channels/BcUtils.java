076c2a88e0873976a448d179c00f1e0e7284a1ed
Jin Kwon <onacit@gmail.com>
2016-02-20 16:32:20 +0900

    updated

---
+++ verbose-java-utils/src/main/java/com/github/jinahya/verbose/nio/channels/BcUtils.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2016 Jin Kwon &lt;jinahya_at_gmail.com&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jinahya.verbose.nio.channels;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import static java.lang.reflect.Proxy.newProxyInstance;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import static java.util.concurrent.ThreadLocalRandom.current;
+
+/**
+ * Utilities for byte channels.
+ *
+ * @author Jin Kwon &lt;jinahya_at_gmail.com&gt;
+ */
+public final class BcUtils {
+
+    /**
+     * Creates a new proxy instance which intercepts
+     * {@link WritableByteChannel#write(java.nio.ByteBuffer)} method and mimics
+     * non-blocking writing by temporarily adjusting the
+     * {@link ByteBuffer#remaining()} of the buffer.
+     *
+     * @param <T> channel type parameter
+     * @param type channel type
+     * @param channel the channel
+     * @return a new proxy instance
+     */
+    public static <T extends WritableByteChannel> T newNonBlockingProxy(
+            final Class<T> type, final T channel) {
+        final Method method; // <1>
+        try {
+            method = WritableByteChannel.class.getMethod(
+                    "write", ByteBuffer.class);
+        } catch (final NoSuchMethodException nsme) {
+            throw new RuntimeException(nsme);
+        }
+        final InvocationHandler handler = (p, m, a) -> {
+            if (method.equals(m)) {
+                final ByteBuffer src = (ByteBuffer) a[0]; // <1>
+                final int limit = src.limit(); // <2>
+                try {
+                    src.limit(src.position() // <3>
+                              + current().nextInt(src.remaining() + 1));
+                    return channel.write(src); // <4>
+                } finally {
+                    src.limit(limit); // <5>
+                }
+            }
+            return m.invoke(channel, a); // <6>
+        };
+        final Object proxy = newProxyInstance( // <1>
+                type.getClassLoader(), new Class<?>[]{type}, handler);
+        return type.cast(proxy); // <2>
+    }
+
+    private static <T extends WritableByteChannel> T newNonBlockingProxyHelper(
+            final Class<T> type, final WritableByteChannel channel) {
+        return BcUtils.newNonBlockingProxy(type, type.cast(channel));
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends WritableByteChannel> T newNonBlockingProxy(
+            final T channel) {
+        return (T) newNonBlockingProxyHelper(channel.getClass(), channel);
+    }
+
+    /**
+     * Creates a new proxy instance which intercepts the invocation of
+     * {@link ReadableByteChannel#read(java.nio.ByteBuffer)} and mimics
+     * non-blocking reading by temporarily adjusting the
+     * {@link ByteBuffer#remaining()} of the buffer.
+     *
+     * @param <T> channel type parameter
+     * @param type channel type
+     * @param channel the channel
+     * @return a new proxy
+     */
+    public static <T extends ReadableByteChannel> T newNonBlockingProxy(
+            final Class<T> type, final T channel) {
+        final Method method; // <1>
+        try {
+            method = ReadableByteChannel.class.getMethod(
+                    "read", ByteBuffer.class);
+        } catch (final NoSuchMethodException nsme) {
+            throw new RuntimeException(nsme);
+        }
+        final InvocationHandler handler = (p, m, a) -> { // <1>
+            if (method.equals(m)) { // <2>
+                final ByteBuffer dst = (ByteBuffer) a[0]; // <3>
+                final int limit = dst.limit(); // <4>
+                try {
+                    dst.limit(dst.position() // <5>
+                              + current().nextInt(dst.remaining() + 1));
+                    return channel.read(dst); // <6>
+                } finally {
+                    dst.limit(limit); // <7>
+                }
+            }
+            return m.invoke(channel, a); // <8>
+        };
+        final Object proxy = newProxyInstance( // <1>
+                type.getClassLoader(), new Class<?>[]{type}, handler);
+        return type.cast(proxy); // <2>
+    }
+
+    private static <T extends ReadableByteChannel> T newNonBlockingProxyHelper(
+            final Class<T> type, final ReadableByteChannel channel) {
+        return BcUtils.newNonBlockingProxy(type, type.cast(channel));
+    }
+
+    /**
+     * Creates a new proxy which intercepts the invocation of
+     * {@link ReadableByteChannel#read(java.nio.ByteBuffer)} and mimics
+     * non-blocking channel by temporarily adjusting the
+     * {@link ByteBuffer#remaining()} of the buffer.
+     *
+     * @param <T> channel type parameter
+     * @param channel the channel
+     * @return a new proxy
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends ReadableByteChannel> T newNonBlockingProxy(
+            final T channel) {
+        return (T) newNonBlockingProxyHelper(channel.getClass(), channel);
+    }
+
+    private BcUtils() {
+        super();
+    }
+}
--- verbose-java-utils/src/test/java/com/github/jinahya/verbose/security/MdUtilsTest.java
+++ verbose-java-utils/src/test/java/com/github/jinahya/verbose/security/MdUtilsTest.java
@@ -15,10 +15,58 @@
  */
 package com.github.jinahya.verbose.security;
 
+import static com.github.jinahya.verbose.security.MdUtils.digest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import static java.nio.ByteBuffer.allocate;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static java.nio.file.StandardOpenOption.WRITE;
+import java.security.NoSuchAlgorithmException;
+import static java.util.concurrent.ThreadLocalRandom.current;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 /**
+ * A class for testing {@link MdUtils}.
  *
  * @author Jin Kwon &lt;jinahya_at_gmail.com&gt;
  */
 public class MdUtilsTest {
 
+    @DataProvider
+    private Object[][] algorithms() {
+        return new Object[][]{{"MD5"}, {"SHA-1"}, {"SHA-256"}};
+    }
+
+    @Test(dataProvider = "algorithms", invocationCount = 128)
+    public static void digestFile(final String algorithm)
+            throws IOException, NoSuchAlgorithmException {
+        final File file = File.createTempFile("tmp", null);
+        file.deleteOnExit();
+        try (OutputStream s = new FileOutputStream(file)) {
+            final byte[] b = new byte[current().nextInt(1048576)];
+            current().nextBytes(b);
+            s.write(b);
+            s.flush();
+        }
+        final byte[] digest = digest(file, algorithm);
+    }
+
+    @Test(dataProvider = "algorithms", invocationCount = 128)
+    public static void digestPath(final String algorithm)
+            throws IOException, NoSuchAlgorithmException {
+        final Path path = Files.createTempFile(null, null);
+        try (FileChannel c = FileChannel.open(path, WRITE)) { // <2>
+            final ByteBuffer b = allocate(current().nextInt(1048576));
+            current().nextBytes(b.array());
+            for (; b.hasRemaining(); c.write(b));
+            c.force(false);
+        }
+        final byte[] digest = digest(path, algorithm);
+    }
 }
